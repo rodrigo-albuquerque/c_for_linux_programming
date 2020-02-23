4048ea4403c1222ba45e2b91e5d08d873cec1dc6
rodrigo-albuquerque <digofarias@gmail.com>
2020-02-23 16:53:41 +0000

    edited pa.c

--- 02_pointers_arrays_and_structs/pa.c
+++ 02_pointers_arrays_and_structs/pa.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 
 int main (int argc, char *argv[]) {
-  int array[5];
-  int number;
-  int *p = array;
-  array = number;
+  int A[5];
+  printf("A's memory address: %p\n", &A);
+  int n;
+  printf("n's memory address: %p\n", &n);
+  int *p = A; // p is now pointing to same memory address of A
+  printf("p is now pointing to %p which is A's memory address\n", p);
+  p = &n;     // this is fine as pointers can be re-assigned to different memory address
+  printf("p now points to %p which is n's memory address\n", p);
+  //A = n;     // this is not
 }
